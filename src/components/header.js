c5b1a8e118d3203b89f771df968dcede8f658a78
Alex Dollard <alexdollard141@gmail.com>
2020-10-02 11:03:06 -0500

    redid header to portfolio in gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -2,28 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import './header.css'
+
 const Header = ({ siteTitle }) => (
   <header
+    className="header"
     style={{
-      background: `rebeccapurple`,
+      // background: '#28b485',
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    <div className="header__text-box">
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link className="header-primary" to="/" >
           {siteTitle}
         </Link>
       </h1>
