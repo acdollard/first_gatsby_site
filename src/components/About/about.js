ab74b77b179fd1db833cb1357452a5533fd85c44
Alex Dollard <alexdollard141@gmail.com>
2020-10-12 16:23:38 -0500

    finished basics for about component

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -5,14 +5,14 @@ import '../../css/main.css'
 const About = () => {
 
     return (
-        <div className="about--container">
-            <div className="u-center-text u-margin-bottom-big">
+        <div className="about__container">
+            <div className="about__heading">
                 <h2 className="heading-secondary">
                     Exciting tours for adventurous people
                 </h2>
             </div>  
-            <div className="about--text">
-                <p className="paragraph-text">
+            <div className="about__text">
+                <p className="about__text--paragraph">
                     My name is alex dollard and this is some dummy text that I can use 
                     in place of real text for now. I'm learning React and building a portfolio
                     so I can show off my fancy shiny cool new programming skills. Chicks only dig 
