1d4ac3e9d1707be5cd0a2c19fb2c6761ac50d6dc
Alex Dollard <alexdollard141@gmail.com>
2020-11-09 19:21:47 -0600

    pre deploy push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+
+
 module.exports = {
   siteMetadata: {
     title: `Alex Dollard`,
@@ -20,6 +26,7 @@ module.exports = {
     // },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -51,9 +58,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: "localhost:10038/alexdollard",
+        baseUrl: process.env.API_URL,
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: process.env.API_PROTOCOL,
         // The rest api route prefix that your WordPress site is using.
         // Sometimes this is modified by WordPress plugins.
         // If not set, it uses the default of "wp-json"
