ef0c7d3fd22afbca9c63d281a0f0e9e0a4c13012
Alex Dollard <alexdollard141@gmail.com>
2020-12-11 17:52:23 -0600

    trying something to solve copy/paste blog text issue

--- src/templates/postPage.js
+++ src/templates/postPage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Link} from 'gatsby'
-import styled from 'styled-components'
 import '../css/main.css';
 
 import Footer from "../components/Footer/footer"
@@ -11,7 +9,8 @@ const postPage = ({ pageContext }) => (
   <Header className="header header_2" siteTitle={"blog"} linkTo={'/blog'} destination={'back to posts'} />
   <div className="blogPostDiv">
     <h1 className="heading-secondary" dangerouslySetInnerHTML={{__html: pageContext.posts.title}} />
-    <p dangerouslySetInnerHTML={{__html: pageContext.posts.content}} />
+
+    <div dangerouslySetInnerHTML={{__html: pageContext.posts.content}} ></div>
 
   </div>
   <Footer className="footer footer_2" />
