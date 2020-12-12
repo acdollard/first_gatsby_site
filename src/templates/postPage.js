920617ffa2ba8e6bdd93321f985ba74c38a914a9
Alex Dollard <alexdollard141@gmail.com>
2020-12-11 18:41:17 -0600

    fixed button spacing issue and added buttons to bottom of blog posts

--- src/templates/postPage.js
+++ src/templates/postPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../css/main.css';
+import {Link} from 'gatsby'
 
 import Footer from "../components/Footer/footer"
 import Header from '../components/Header/header'
 
 const postPage = ({ pageContext }) => (
   <>
-  <Header className="header header_2" siteTitle={"blog"} linkTo={'/blog'} destination={'back to posts'} />
+  <Header className="header header_2" siteTitle={"blog"} linkTo={'/blog'} destination={'back'} />
   <div className="blogPostDiv">
     <h1 className="heading-secondary" dangerouslySetInnerHTML={{__html: pageContext.posts.title}} />
 
     <div dangerouslySetInnerHTML={{__html: pageContext.posts.content}} ></div>
 
+    <Link className="btn-small" to={'/blog'}>Back</Link>
   </div>
   <Footer className="footer footer_2" />
   </>
