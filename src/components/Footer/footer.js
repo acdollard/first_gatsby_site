9c727758e821b31ad6cc3ce80705ab852593f3de
alexDollard <alexdollard141@gmail.com>
2021-01-11 20:02:11 -0600

    added icons and copyright to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import '../../css/main.css'
+import GithubIcon from '../../images/github-icon.svg';
+import LinkedInIcon from '../../images/linkedin-icon.svg';
+import EmailIcon from '../../images/email-icon-2.svg';
+
 
 const Footer = ({ className }) => {
 
     return (
     <div className={className}>
-        <div className="contact-info">
-            <p className="background-quote footer__contact-info">alexdollard141@gmail.com</p>
-
+        <div className="footer__flexbox">
+            <div className="footer__social-links">
+                <a className="footer__social-link" href="#">
+                    <img alt="email" src={EmailIcon}></img>
+                </a>
+                <a className="footer__social-link" href="https://github.com/acdollard">
+                    <img alt="github" src={GithubIcon} />
+                </a>
+                <a className="footer__social-link" href="https://www.linkedin.com/in/alex-dollard-a1130aa8/">
+                    <img alt="linkedin" src={LinkedInIcon}></img>
+                </a>
+            </div>
+            <p className="footer__copyright">Copyright &copy; Alex Dollard 2021</p>
+            
         </div>
     </div>
     )
