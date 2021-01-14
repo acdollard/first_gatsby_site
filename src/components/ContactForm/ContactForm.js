2901902b6cd9cb94f8860c8ce36e029196469ff2
alexDollard <alexdollard141@gmail.com>
2021-01-14 13:04:31 -0600

    contact form state updating correctly on submit / looks presentable on mobile

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,17 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "gatsby";
 import '../../css/main.css';
+import axios from 'axios'
 
 import SectionHeading from '../SectionHeading/SectionHeading'
 
 const ContactForm = () => {
 
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const submitEmail = (e) => {
+        console.log("Your message:" + message)
+        e.preventDefault();
+        // axios({
+        //     method: "POST",
+        //     url: "/send",
+        //     data: { 
+        //         name: name,
+        //         email: email,
+        //         message: message
+        //     }
+        // }).then((response) => {
+        //     if (response.data.status === 'success') {
+        //         alert("Message sent! Thank you");
+        //         resetForm();
+        //     } else if (response.data.status === 'fail') {
+        //         alert("Message failed to send")
+        //     }
+        // })
+    }
+
+    const resetForm = () => {
+        setName("");
+        setEmail("");
+        setMessage("");
+    }
+
+    useEffect(() => {
+        console.log({ name: name, email: email, message: message});
+    }, [name, email, message])
+
 
     return (
  
 
-        <div className="contactForm" id="contactForm">
+        <form className="contactForm" id="contactForm" onSubmit={submitEmail}>
             <div className="row__contact">
             <div className="">
         <SectionHeading title={'Contact me'} color={"white"}/>
@@ -19,17 +54,35 @@ const ContactForm = () => {
             </div>
             <div className="row__contact">
                 <div className="col-1-of-2">
-                    <input placeHolder="name" className="textInput"/>
+                    <input 
+                        placeHolder="name" 
+                        className="textInput" 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value) }
+                        />
                 </div>
                 <div className="col-1-of-2">
-                    <input placeHolder="email" className="textInput"/>
+                    <input 
+                        placeholder="email" 
+                        className="textInput"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
                 </div>
             </div>
             <div className="row__contact" >
-                    <textarea placeholder="message" className="textField"/>
+                    <textarea 
+                        placeholder="message" 
+                        className="textField"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        />
             </div>
-            <Link to={"#"} type="submit" className="btn btn__contact">Submit</Link>
-        </div>
+            <div className="row__contact">
+                <button type="submit" className="btn btn__contact"  value="Submit">Submit</button>
+
+            </div>
+        </form>
     
     )
 };
