ecf49b6cce4341a21d6524fb8355992bfa60ff9c
alexDollard <alexdollard141@gmail.com>
2021-01-22 11:10:53 -0600

    switched to netlify's form submission

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,91 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from "gatsby";
+import React, { useState } from 'react';
 import '../../css/main.css';
-import axios from 'axios'
 
 import SectionHeading from '../SectionHeading/SectionHeading'
 
 const ContactForm = () => {
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [message, setMessage] = useState("");
+    // const [subject, setSubject] = useState("");
 
-    const submitEmail = (e) => {
-        console.log("Your message:" + message)
-        e.preventDefault();
-        axios({
-            method: "POST",
-            url: "/send",
-            data: { 
-                name: name,
-                email: email,
-                message: message
-            }
-        }).then((response) => {
-            console.log("hi!");
-            console.log(response);
-            if (response.status === 200) {
-                alert("Message sent! Thank you");
-                resetForm();
-            } else if (response.data.status === 'fail') {
-                alert("Message failed to send")
-            }
-        })
-    }
+    // const submitEmail = (e) => {
+    //     console.log("Your message:" + message)
+    //     e.preventDefault();
+    //     axios({
+    //         method: "POST",
+    //         url: "/send",
+    //         data: { 
+    //             name: name,
+    //             email: email,
+    //             message: message
+    //         }
+    //     }).then((response) => {
+    //         console.log("hi!");
+    //         console.log(response);
+    //         if (response.status === 200) {
+    //             alert("Message sent! Thank you");
+    //             resetForm();
+    //         } else if (response.data.status === 'fail') {
+    //             alert("Message failed to send")
+    //         }
+    //     })
+    // }
 
-    const resetForm = () => {
-        setName("");
-        setEmail("");
-        setMessage("");
-    }
+    // const resetForm = () => {
+    //     setName("");
+    //     setEmail("");
+    //     setMessage("");
+    //     console.log("form reset")
+    // }
 
-    useEffect(() => {
-        console.log({ name: name, email: email, message: message});
-    }, [name, email, message])
+    // useEffect(() => {
+    //     console.log({ name: name, email: email, message: message});
+    // }, [name, email, message])
 
 
     return (
- 
-
-        <form className="contactForm" id="contactForm" onSubmit={submitEmail}>
+        <form 
+            className="contactForm" 
+            id="contactForm" 
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            name="contact"
+        >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div className="row__contact">
-            <div className="">
-        <SectionHeading title={'Contact me'} color={"white"}/>
-        </div>
+                <SectionHeading title={'Contact me'} color={"white"}/>
             </div>
             <div className="row__contact">
                 <div className="col-1-of-2">
                     <input 
-                        placeHolder="your name" 
+                        type="text"
+                        placeholder="your name" 
                         className="textInput" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value) }
+                        // value={name} 
+                        // onChange={(e) => setName(e.target.value) }
                         />
                 </div>
                 <div className="col-1-of-2">
                     <input 
+                        type="text"
                         placeholder="your email" 
                         className="textInput"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        // value={email}
+                        // onChange={(e) => setEmail(e.target.value)}
                         />
                 </div>
             </div>
             <div className="row__contact" >
+                <input
+                    type="text"
+                    placeholder="subject"
+                    className="textInput"
+                    // value={subject}
+                    // onChange={(e)=> setSubject(e.target.value)}
+                    />
+
+            </div>
+                <div className="row__contact" >
                     <textarea 
                         placeholder="message" 
                         className="textField"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
+                        // value={message}
+                        // onChange={(e) => setMessage(e.target.value)}
                         />
-            </div>
+                </div>
             <div className="row__contact">
-                <button type="submit" className="btn btn__contact"  value="Submit">Submit</button>
-
+                <button type="submit" className="btn btn__contact" >Submit</button>
             </div>
         </form>
-    
     )
 };
 
